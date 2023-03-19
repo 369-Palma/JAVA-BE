60e33c7fcfaefafe84c5fc8a6bf2ec4574b256d4
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-19 22:28:38 +0100

    finito

--- prova/src/progetto/settimana1/playerMultimediale/lettoreMultimediale.java
+++ prova/src/progetto/settimana1/playerMultimediale/lettoreMultimediale.java
@@ -1,103 +1,155 @@
-//package progetto.settimana1.playerMultimediale;
-//
-//import java.util.Scanner;
-//
-//public class lettoreMultimediale {
-//	public static void main(String[] args) {
-//		String arrOpzioni[] = new String [1];
-//		arrOpzioni[0] = "0";
-//	private static int contatore = 0;
-//
-//	
-//	
-//		//Configurazione dispositivi
-//		
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("*                                                                  *");
-//		System.out.println("*  Scegli dispositivo: immagine, audio, video  *");
-//		System.out.println("*                                                                  *");	
-//		String dispositivo = scanner.next().toLowerCase();
-//		
-//		System.out.println("*                                                                  *");
-//		System.out.println("*  Inserisci titolo e premi invio *");
-//		System.out.println("*                                                                  *");
-//		String titoloInput = scanner.next();
-//			
-//		System.out.println("*  Scegli durata: digita un numero e premi invio *");
-//		System.out.println("*                                                                  *");
-//		int durata = scanner.nextInt();
-//		
-//		System.out.println("*                                                                  *");
-//		System.out.println("*  Regola la luminosità inserendo un numero e premi invio  *");
-//		System.out.println("*                                                                  *");
-//		int luminosita = scanner.nextInt();
-//		
-//		System.out.println("*                                                                  *");
-//		System.out.println("*  Regola il volume inserendo un numero e premi invio  *");
-//		System.out.println("*                                                                  *");
-//		int volume = scanner.nextInt();
-//		scanner.close();
-//	
-//	Immagine img = new Immagine (titoloInput, durata, luminosita);
-//	Audio audio = new Audio (titoloInput, durata, volume);
-//	Video video = new Video (titoloInput, durata, luminosita, volume);
-//	
-//	esecuzione(dispositivo,durata, img, video,audio);
-//	}
-//	
-//	
-////array di 5 elementi scelti dall'utente
-//	String utenteArr [][]=new String [1][];
-//	
-//	
-////push elemento nell'array
-//	String arr2[]=new String [utenteArr.length +1];
-//	
-//	
-//	//funzione:  controllo sul tipo di dispositivo scelto per far partire metodo play() o show()
-//	
-//	public static void esecuzione(String dispositivo, int durata, Immagine img,Video video,Audio audio) { 
-//		
-//		
-//	
-//		
-//	for(int k=0; k<=arrOpzioni.length; k++) {
-//		
-//	}
-//	Scanner scanner = new Scanner(System.in);
-//	System.out.println("*                                                                  *");
-//	System.out.println("*  Dispositivi configurati correttamente! Scegli un numero da 1 a 5 per vedere la tua scelta oppure premi 0 per uscire.  *");
-//	System.out.println("*                                                                  *");
-//	int sceltaUtente = scanner.nextInt();
-//	scanner.close();
-//	
-//	
-//	
-//	
-//	
-//	
-//	while (sceltaUtente !=0) {
-//		
-//	for(int i = 0; i<=5; i++) {
-//	switch (dispositivo) {
-//		case "immagine":
-//			img.show();
-//			break;
-//		case "audio":
-//			audio.play();
-//			break;
-//		case "video":
-//			video.show();
-//			video.play();
-//			break;
-//			
-//		}	
-//	}
-//	}	
-//	
-//	
-//	
-//	
-//	}
-//	
-//}
+package progetto.settimana1.playerMultimediale;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class lettoreMultimediale {
+	
+	
+static int  arrDispositivi[] = new int[5];
+		static int [] newArrDispositivi[];
+		
+	public static void main(String[] args) {
+		
+		getDispositivi();
+		eseguiRichiesta();
+	}
+	
+	//funzioni 
+	
+	//crea Array
+	public static int[] getDispositivi() {
+	
+		
+		for(int i=0; i<=4; i++) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Scegli un numero da 1 a 3 dove 1 è immagine, 2 è audio e 3 è video. Poi premi invio.");
+		System.out.println("");
+		
+		int response = scanner.nextInt();
+		
+		while(response < 1 || response > 3) {
+			System.out.println("Scelta non valida!");
+			Scanner scanner1 = new Scanner(System.in);
+			System.out.print("Scegli un numero da 1 a 3 dove 1 è immagine, 2 è audio e 3 è video. Poi premi invio.");
+			System.out.println("");
+			
+			response = scanner1.nextInt();
+		
+		}
+		arrDispositivi[i] = response;
+		}
+		System.out.println(Arrays.toString(arrDispositivi));
+		System.out.println("Dispositivi configurati!");
+		
+		return arrDispositivi;
+		
+		}
+	
+	// EseCuzione dei dispositivi su richiesta dell'utente
+	
+public static void eseguiRichiesta() {
+		
+		int res=1;
+		while(res>0) {
+			
+			Scanner scanner = new Scanner(System.in);
+			//res=1;
+			System.out.println("");
+			System.out.println("*  Scegli un numero da 1 a 5 oppure premi 0 per uscire.  *");
+			System.out.println("");
+			res = scanner.nextInt();
+			
+			if(res < 1) {
+				System.out.println("* Arrivederci! *");
+				break;
+			}
+			switch(arrDispositivi[res-1]) {
+			
+			case 1:
+				Scanner scanner1 = new Scanner(System.in);
+				System.out.println("");
+				System.out.println("*  Hai scelto un'immagine  *");
+				System.out.println("");					
+				
+				System.out.println("");
+				System.out.println("*  Inserisci titolo e premi invio *");
+				System.out.println("");
+				String titoloInput = scanner1.next();
+				
+				System.out.println("");
+				System.out.println("*  Regola la luminosità inserendo un numero e premi invio  *");
+				System.out.println("");
+				int luminosita = scanner1.nextInt();
+				
+				
+				Immagine img = new Immagine (titoloInput, luminosita);
+				
+				img.show();
+				
+				break;
+				
+				
+			case 2:
+				Scanner scanner2 = new Scanner(System.in);
+				System.out.println("");
+				System.out.println("*  Hai scelto un audio  *");
+				System.out.println("");					
+				
+				System.out.println("");
+				System.out.println("*  Inserisci titolo e premi invio *");
+				System.out.println("");
+				String titoloInput2 = scanner2.next();
+					
+				System.out.println("*  Scegli durata: digita un numero e premi invio *");
+				System.out.println("");
+				int durata = scanner2.nextInt();
+				
+				System.out.println("");
+				System.out.println("*  Regola il volume inserendo un numero e premi invio  *");
+				System.out.println("");
+				int volume = scanner2.nextInt();
+				
+				Audio audio = new Audio (titoloInput2, durata, volume);
+				audio.play();
+				
+				
+				break;
+				
+			case 3: 
+				Scanner scanner3 = new Scanner(System.in);
+				System.out.println("");
+				System.out.println("*  Hai scelto un video  *");
+				System.out.println("");					
+				
+				System.out.println("");
+				System.out.println("*  Inserisci titolo e premi invio *");
+				System.out.println("");
+				String titoloInput3 = scanner3.next();
+					
+				System.out.println("*  Scegli durata: digita un numero e premi invio *");
+				System.out.println("");
+				int durata3 = scanner3.nextInt();
+				
+				System.out.println("");
+				System.out.println("*  Regola la luminosità inserendo un numero e premi invio  *");
+				System.out.println("");
+				int luminosita3 = scanner3.nextInt();
+				
+				System.out.println("");
+				System.out.println("*  Regola il volume inserendo un numero e premi invio  *");
+				System.out.println("");
+				int volume3 = scanner3.nextInt();
+				
+				
+				Video video = new Video (titoloInput3, durata3, luminosita3, volume3);
+				video.play();
+				
+				break;
+	
+			}
+			
+		}
+	}
+
+}
