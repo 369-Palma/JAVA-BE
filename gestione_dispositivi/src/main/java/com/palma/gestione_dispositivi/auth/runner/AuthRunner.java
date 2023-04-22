ce9adcec080fdaee85dd3941aad790baf8b4c916
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-04-23 00:17:18 +0200

    .

--- gestione_dispositivi/src/main/java/com/palma/gestione_dispositivi/auth/runner/AuthRunner.java
+++ gestione_dispositivi/src/main/java/com/palma/gestione_dispositivi/auth/runner/AuthRunner.java
@@ -1,9 +1,7 @@
 package com.palma.gestione_dispositivi.auth.runner;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
